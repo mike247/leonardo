65b0e7cecf96a6db83490f89df3054915e834ed9
Michael Norrie <mike.ski247@gmail.com>
2024-10-11 11:38:46 +1300

    fancier character widget

--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Avatar, Text } from "@chakra-ui/react";
+import { Box, Flex, Badge, Text, Image } from "@chakra-ui/react";
 
 export type Character = {
   gender: string;
@@ -19,19 +19,42 @@ export default function CharacterWidget({
 }: CharacterWidgetProps) {
   return (
     <Box
-      m="3"
+      m="1"
       border="1px"
       borderRadius="5"
       borderColor="#ddd"
-      minW="300"
+      w="200"
       onClick={onClick}
+      position="relative"
+      overflow="hidden"
+      role="group"
     >
-      <Flex m="2">
-        <Avatar src={character.image} />
-        <Flex ml="3" alignItems={"center"}>
-          <Text fontWeight="bold">{character.name}</Text>
+      <Image src={character.image} />
+      <Box
+        position="absolute"
+        bottom={{ base: 0, md: -35 }}
+        transition="0.3s"
+        width="100%"
+        background={{ base: "rgba(100, 100, 100, 0.8)", md: "none" }}
+        _groupHover={{ bottom: "0", background: "rgba(100, 100, 100, 0.8)" }}
+      >
+        <Flex ml="2" justifyContent="center" flexDirection="column">
+          <Text
+            fontWeight="bold"
+            mt="2"
+            color="white"
+            textShadow="2px 2px 10px #000"
+          >
+            {character.name}
+          </Text>
+          <Text fontSize="sm" mt="2" mb="2" color="white">
+            {character.species}
+            <Badge ml="2" colorScheme="green">
+              {character.gender}
+            </Badge>
+          </Text>
         </Flex>
-      </Flex>
+      </Box>
     </Box>
   );
 }
